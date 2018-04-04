a3dffbf564a36b67eb4d142706c15d7484a85c69
Megan Dotto <megan.dotto@gmail.com>
2018-04-04 04:40:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
 
   @@count = 0
   @@artists = []
+  @@genres = []
 
 def initialize(name, artist, genre)
   @name = name
@@ -10,6 +11,7 @@ def initialize(name, artist, genre)
   @genre = genre
   @@count += 1
   @@artists << artist
+  @@genres << genre
 end
 
 end
